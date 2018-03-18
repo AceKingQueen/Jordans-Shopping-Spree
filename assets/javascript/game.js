68843bb5fc3f92ec2e6cacfdb8be5187045955a5
Allison Williams <allisonwilliams@Allisons-MacBook.local>
2018-03-18 15:26:59 -0400

    basic Bootstap styling set up

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+/* - make images clickable but not links to anything
+    - annual bonus given a random amount
+    - each pair of jordans given a random cost
+    - be able to add one pairs cost to the next
+    - if sum > annual bonus, game over LOSS & loss++
+    - if sum = annual bonus, game over WIN & win++
