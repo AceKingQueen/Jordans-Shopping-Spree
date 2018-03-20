296782f836db9300b87708c942346bec3898fa67
Allison Williams <allisonwilliams@Allisons-MacBook.local>
2018-03-19 23:06:50 -0400

    mostly working, tweaks needed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,19 @@
 $(document).ready(function() {
 
 //variable to track user's total
+var annualBonus = 0;
+
 var counter = 0;    
 
 var wins = 0;
 
 var losses = 0;
+
+var shoes = $("#shoes");
+
+//each pair of Jordans given a random number chosen from this array
+var jordanPrice = [100, 10, 5, 1];
+
 
 //annual bonus given a random amount on click of button
 $("#bonus-button").on("click", function() {
@@ -14,77 +22,59 @@ $("#bonus-button").on("click", function() {
     //annual bonus given a random amount between 500 and 1000   
     var annualBonus = Math.floor(Math.random() * 1000) + 500;
     
-
     //annual bonus to appear on html
     $("#bonus-amount").append(annualBonus);
 
-    //each pair of Jordans given a random number assignment between 10 and 100
-    var jordanCost = Math.floor(Math.random() * 100) + 10;
+
+    //for loop to assign values for each Jordan pair
+    for (var j = 0; j < jordanPrice.length; j++) {
+
+        //create var for each Jordan
+        var imageShoe = $("<img>");
+
+        //each var given class value using method
+        imageShoe.addClass("shoe-image");
+
+        //each var given src link to image
+        imageShoe.attr("src", "assets/images/jordan-1.png");
+
+        //each var given data attribute equal to array value of jordanCost
+        imageShoe.attr("data-jordancost", jordanPrice[j]);
+
+        //each image with all it's classes and attributes added to page
+        shoes.append(imageShoe);
+    } 
+
+shoes.on("click", ".shoe-image", function() {
+
+
 
     //make images clickable
-    $(".shoe-image").on("click", function() {
+    // $(".shoes").on("click", function() {
 
-       counter += 10; 
+        var jordanCost = ($(this).attr("data-jordancost"));
+        jordanCost = parseInt(jordanCost);
 
-       alert("You clicked this shoe " + counter + "times.");
+       counter += jordanCost; 
 
+       alert("New score: " + counter);
+       //checking to see if total amount is equal to annualBonus 
+       if (counter === annualBonus) {
 
+           alert("You win!");
+           wins++;
+
+       //add else if condition    
+       } else if (counter >= annualBonus) {
+
+           alert("You lose");
+           losses++;
+       }
     })
 
-  
+})
     })
 
+$("#wintotal").text(wins);
 
-      
-
-        //output how many times the shoe is clicked
-        // $(".clicks").append(counter);
-
-        //if (counter === annualBonus) {
-        //     wins++;
-        //     alert("You win!");
-        // } else if (counter > annualBonus) {
-            // losses++;
-            // alert("You lose!")
-        
-
-
-
-
-
-    // $("#ones").append(jordanCost);
-
-
-    })
-
-
-
-  //function newGame() {
-//       wraps whole game together; keep things in this
-//   }
-
-
-
-
-
-
-/*
-
-    - each pair of jordans given a random cost
-
-
-
-    - be able to add one pairs cost to the next
-    - if sum > annual bonus, game over LOSS & loss++
-    - if sum = annual bonus, game over WIN & win++
-    */
-
-
-
-    // function startGame() {
-
-    // }
-
-    // wins and losses happen outside of game loop because they aren't changing; just keep going forever.
-
-    // start counts at 0 for random generator
+$("#losses").text(losses);
